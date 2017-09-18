b47bcac1269ea8f14544cd7f6a12e0d4c76fe589
glt79 <glt79@msstate.edu>
2017-09-18 01:44:38 -0500

    Setup the Init and Method for Item Class

--- Item.py
+++ Item.py
@@ -1,3 +1,9 @@
 class Item:
 	def __init__():
 		self.name = ""
+                self.itemId = itemId
+		self.price = price
+		self.quantity = quantity
+		self.description = ""
+	def getFeatureDescription():
+		pass
