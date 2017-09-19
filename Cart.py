9d07f2d1cf6af838658044b0e3065a1c2c905eb4
glt79 <glt79@msstate.edu>
2017-09-19 16:49:43 -0500

    Simple Skeleton of Cart() Class

--- Cart.py
+++ Cart.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
 
-class Cart():
-	pass
+class Cart(items):
+	def __init__():
+		items = []
+		id
+
+	def addItems(items):
+		return true
+
+	def isEmpty():
+		return true
+
+	def removeItems(items):
+		return true
+
+
