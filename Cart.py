1c159152514ea50fcf142f8d715bb4d5d8501d8d
glt79 <glt79@msstate.edu>
2017-09-20 10:41:51 -0500

    All of Cart Class Except for getTotalPrice

--- Cart.py
+++ Cart.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python
 
-class Cart(items):
-	def __init__():
-		items = []
-		id
+class Cart:
+	def __init__(self, items):
+		self.items = items
+		self.cartId = cartId
 
-	def addItems(items):
+	def addItems(newitems):
+		for each in newitems:
+			self.items.append(newitems)
+
 		return true
 
 	def isEmpty():
+		if self.items[0] == null:
+			return true
+		else:
+			return false
+
+	def removeItems(removalItems):
+
+		for each in self.items:
+			if removalItems in self.items:
+				self.items.remove(removalItems)
+				
 		return true
 
-	def removeItems(items):
-		return true
+	def getTotalQuantity():
+		numItems = 0
+
+		for each in self.items:
+			numItems += 1
+
+		return numItems
+
+	def getTotalPrice(items):
+
+		return
 
 
